ddceee1179ed8a22d20b629995f73fdaf7e5ce5e
IsraaHossam <104870243+IsraaHossam@users.noreply.github.com>
2024-12-18 04:56:02 +0200

    Delete ImperativeFinancialApp directory

--- ImperativeFinancialApp/ImperativeFinancialApp/Models/FinancialData.cs
+++
@@ -1,9 +0,0 @@
-namespace ImperativeFinancialApp.Models
-{
-    public class FinancialData
-    {
-        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
-        public List<Budget> Budgets { get; set; } = new List<Budget>();
-        public List<SavingGoal> SavingGoals { get; set; } = new List<SavingGoal>();
-    }
-}
